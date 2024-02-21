6a913055d366be9481b203257f8ee2a1534baf0b
SIDHIQ ALI <sidhiqsidhiqali@gmail.com>
2024-02-21 16:25:21 +0530

    Update indianRupeeFormat.js

--- src/usefullFunctions/indianRupeeFormat.js
+++ src/usefullFunctions/indianRupeeFormat.js
@@ -3,7 +3,7 @@ export function toRupees (number) {
     let lastThree = number.substring(number.length-3);
     let otherNumbers = number.substring(0,number.length-3);
     if(otherNumbers !== '')
-        lastThree = ',' + lastThree;
+    lastThree = ',' + lastThree;
     let res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
     return res;
 } 
